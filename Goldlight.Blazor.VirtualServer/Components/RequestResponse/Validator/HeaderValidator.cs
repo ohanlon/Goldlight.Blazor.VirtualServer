4401e2739d3d6922101a0b6657fe16ca531792a5
ohanlon <pete.ohanlon@gmail.com>
2023-10-05 13:42:54 +0100

    Branding now starting to look like it belongs to the application, and not to MudBlazor.

--- Goldlight.Blazor.VirtualServer/Components/RequestResponse/Validator/HeaderValidator.cs
+++ Goldlight.Blazor.VirtualServer/Components/RequestResponse/Validator/HeaderValidator.cs
@@ -5,9 +5,9 @@ namespace Goldlight.Blazor.VirtualServer.Components.RequestResponse.Validator;
 
 public class HeaderValidator : AbstractValidator<HttpHeader>
 {
-    public HeaderValidator()
-    {
-        RuleFor(hdr => hdr.Name).NotEmpty();
-        RuleFor(hdr => hdr.Value).NotEmpty();
-    }
+  public HeaderValidator()
+  {
+    RuleFor(hdr => hdr.Name).NotEmpty();
+    RuleFor(hdr => hdr.Value).NotEmpty();
+  }
 }
--- Goldlight.Blazor.VirtualServer/Components/RequestResponse/Validator/RequestResponsePairValidator.cs
+++ Goldlight.Blazor.VirtualServer/Components/RequestResponse/Validator/RequestResponsePairValidator.cs
@@ -1,27 +1,30 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 using Goldlight.Blazor.VirtualServer.Models.RequestResponse;
 
 namespace Goldlight.Blazor.VirtualServer.Components.RequestResponse.Validator;
 
 public class RequestResponsePairValidator : AbstractValidator<RequestResponsePair>
 {
-    public RequestResponsePairValidator()
-    {
-        RuleFor(rr => rr.Name).NotEmpty().MinimumLength(10);
-        RuleFor(rr => rr.Description).NotEmpty().MinimumLength(20);
-        RuleFor(rr => rr.Response.Summary.Status).NotEmpty();
-        RuleFor(rr => rr.Response.Summary.Version).NotEmpty();
-        RuleFor(rr => rr.Request.Summary.Method).NotEmpty();
-        RuleFor(rr => rr.Request.Summary.Path).NotEmpty();
-        RuleForEach(rr => rr.Response.Headers).SetValidator(new HeaderValidator());
-        RuleForEach(rr => rr.Request.Headers).SetValidator(new HeaderValidator());
-    }
+  public RequestResponsePairValidator()
+  {
+    RuleFor(rr => rr.Name).NotEmpty().WithMessage("The name needs a minimum of 10 characters.").MinimumLength(10)
+      .WithMessage("The name needs a minimum of 10 characters.");
+    RuleFor(rr => rr.Description).NotEmpty().WithMessage("The description needs a minimum of 10 characters.")
+      .MinimumLength(10).WithMessage("The description needs a minimum of 10 characters.");
+    RuleFor(rr => rr.Response.Summary.Status).NotEmpty();
+    RuleFor(rr => rr.Response.Summary.Version).NotEmpty();
+    RuleFor(rr => rr.Request.Summary.Method).NotEmpty();
+    RuleFor(rr => rr.Request.Summary.Path).NotEmpty();
+    RuleForEach(rr => rr.Response.Headers).SetValidator(new HeaderValidator());
+    RuleForEach(rr => rr.Request.Headers).SetValidator(new HeaderValidator());
+  }
 
-    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-    {
-        var result = await ValidateAsync(ValidationContext<RequestResponsePair>.CreateWithOptions((RequestResponsePair)model, x => x.IncludeProperties(propertyName)));
-        if (result.IsValid)
-            return Array.Empty<string>();
-        return result.Errors.Select(e => e.ErrorMessage);
-    };
+  public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+  {
+    ValidationResult? result =
+      await ValidateAsync(ValidationContext<RequestResponsePair>.CreateWithOptions((RequestResponsePair)model,
+        context => context.IncludeProperties(propertyName)));
+    return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+  };
 }
