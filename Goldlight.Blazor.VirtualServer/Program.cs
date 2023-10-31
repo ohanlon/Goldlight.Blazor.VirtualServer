7c32583489d2f2f5523185d3af7febbd07f78a20
ohanlon <pete.ohanlon@gmail.com>
2023-10-31 11:42:37 +0000

    We have the ability to log in, and log out, of our application.

--- Goldlight.Blazor.VirtualServer/Program.cs
+++ Goldlight.Blazor.VirtualServer/Program.cs
@@ -3,6 +3,7 @@ using Goldlight.Blazor.VirtualServer.Api;
 using Goldlight.Blazor.VirtualServer.Services;
 using Goldlight.Blazor.VirtualServer.State;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
@@ -16,5 +17,7 @@ builder.Services.AddMudServices();
 
 builder.Services.AddScoped<OrganizationApi>().AddScoped<ProjectApi>().AddScoped<ProjectProps>()
   .AddSingleton<Clipboard>();
+
+builder.Services.AddOidcAuthentication(options => { builder.Configuration.Bind("Keycloak", options.ProviderOptions); });
 
 await builder.Build().RunAsync();
