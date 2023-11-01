4e577ab7fef308c1bf0de00b753efd836637f912
ohanlon <pete.ohanlon@gmail.com>
2023-11-01 10:14:32 +0000

    We now have the ability to automatically add access tokens to any request when the user is logged in.

---
+++ Goldlight.Blazor.VirtualServer/Extensions/TokenAuthorizationMessageHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http.Headers;
+
+namespace Goldlight.Blazor.VirtualServer.Extensions;
+
+public class TokenAuthorizationMessageHandler : DelegatingHandler
+{
+  private readonly Uri baseUri;
+  private readonly IAccessTokenProvider accessTokenProvider;
+  private AuthenticationHeaderValue? cachedAuthenticationHeaderValue;
+  private AccessToken? cachedAccessToken;
+
+  public TokenAuthorizationMessageHandler(IAccessTokenProvider provider,
+    IConfiguration configuration)
+  {
+    baseUri = new Uri(configuration["Server:BaseAddress"]!);
+    accessTokenProvider = provider;
+  }
+
+  protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+    CancellationToken cancellationToken)
+  {
+    if (baseUri.IsBaseOf(request.RequestUri!))
+    {
+      await PopulateCachedAccessTokenIfUnsetOrExpired();
+
+      request.Headers.Authorization = cachedAuthenticationHeaderValue!;
+    }
+
+    return await base.SendAsync(request, cancellationToken);
+  }
+
+  private async Task PopulateCachedAccessTokenIfUnsetOrExpired()
+  {
+    if (cachedAccessToken is null || cachedAccessToken.Expires.AddMinutes(-5) <= DateTime.Now)
+    {
+      AccessToken? accessToken = await RequestAuthToken(accessTokenProvider);
+      if (accessToken is not null)
+      {
+        cachedAccessToken = accessToken;
+        cachedAuthenticationHeaderValue = new AuthenticationHeaderValue("Bearer", accessToken.Value);
+      }
+    }
+  }
+
+  private static async Task<AccessToken?> RequestAuthToken(IAccessTokenProvider tokenProvider)
+  {
+    AccessTokenResult? requestToken = await tokenProvider.RequestAccessToken();
+    requestToken.TryGetToken(out AccessToken? token);
+    return token;
+  }
+}
--- Goldlight.Blazor.VirtualServer/Program.cs
+++ Goldlight.Blazor.VirtualServer/Program.cs
@@ -1,9 +1,9 @@
 using Goldlight.Blazor.VirtualServer;
 using Goldlight.Blazor.VirtualServer.Api;
+using Goldlight.Blazor.VirtualServer.Extensions;
 using Goldlight.Blazor.VirtualServer.Services;
 using Goldlight.Blazor.VirtualServer.State;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
@@ -11,13 +11,25 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-  { BaseAddress = new Uri(builder.Configuration["Server:BaseAddress"]!) });
-builder.Services.AddMudServices();
 
+builder.Services.AddHttpClient("ServerAPI",
+    client => client.BaseAddress = new Uri(builder.Configuration["Server:BaseAddress"]!))
+  .AddHttpMessageHandler<TokenAuthorizationMessageHandler>();
+
+builder.Services.AddScoped<TokenAuthorizationMessageHandler>()
+  .AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("ServerAPI"));
+
+builder.Services.AddMudServices();
 builder.Services.AddScoped<OrganizationApi>().AddScoped<ProjectApi>().AddScoped<ProjectProps>()
   .AddSingleton<Clipboard>();
 
-builder.Services.AddOidcAuthentication(options => { builder.Configuration.Bind("Keycloak", options.ProviderOptions); });
+builder.Services.AddOidcAuthentication(options =>
+{
+  builder.Configuration.Bind("Keycloak", options.ProviderOptions);
+  //options.UserOptions.NameClaim = "preferred_username";
+  //options.UserOptions.RoleClaim = "roles";
+  //options.UserOptions.ScopeClaim = "scope";
+});
 
 await builder.Build().RunAsync();
